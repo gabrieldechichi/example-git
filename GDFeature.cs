74ed67ae10299794a6a16bed85411574f4178374
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-10-10 12:32:03 -0600

    My second commit

--- GDFeature.cs
+++ GDFeature.cs
@@ -1,12 +1,22 @@
 using System;
 
 namespace GitExample
-{	
-    public class GDFeature 
-    {	
+{
+    public class GDFeature
+    {
         public void TestFeature()
-        {	
+        {
             Console.WriteLine("Test!");
+        }
+
+        public void TestFeature2()
+        {
+            Console.WriteLine("Test 2!");
+        }
+
+        public void TestFeature3()
+        {
+            Console.WriteLine("Test 3!");
         }
     }
 }
